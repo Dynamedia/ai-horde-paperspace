25b6302b3dfbe5099d1c166e227e196c8c80e7b8
Rob Ballantyne <rob@dynamedia.uk>
2023-05-26 07:44:13 +0100

    write config before update

--- bin/dreamer.sh
+++ bin/dreamer.sh
@@ -7,11 +7,11 @@ do
     esac
 done
 
-echo "Updating worker and hordelib..."
-update-horde.sh
-
 cd /notebooks
 python -c 'from lib import utils; utils.write_yaml_config()'
+
+echo "Updating worker and hordelib..."
+update-horde.sh
 
 cd /opt/AI-Horde-Worker
 until python bridge_stable_diffusion.py ${quiet}; do
